ef407d6009f75a199ba651057b455a530b90f4aa
Jackson <balestjackson@gmail.com>
2022-12-03 18:00:02 -0300

    Seção 2:DotNet 5

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento() 
+                {
+                    EventoId = 1,
+                    Tema = "Festa do jack",
+                    Local = "Belo Horizonte",
+                    Lote = "1° Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString()
+                },
+                new Evento() 
+                {
+                    EventoId = 2,
+                    Tema = "Festa do Ju",
+                    Local = "Poa",
+                    Lote = "3° Lote",
+                    QtdPessoas = 450,
+                    DataEvento = DateTime.Now.AddDays(4).ToString()
+                }
+            };
+
+        public EventoController()
+        {
+            
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetByid(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo Delete com id = {id}";
+        }
+
+    }
+}
