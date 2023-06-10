ab341f4c50f372fffb59a560a3891fa58113463e
Jackson <balestjackson@gmail.com>
2023-06-09 22:18:11 -0300

    Subida antes de atualizar

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
